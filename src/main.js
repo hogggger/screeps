070ac32a89d3ec47bbc1cab8ff37131b6c03a200
hogger <deuntlt@vip.qq.com>
2022-04-23 15:37:00 +0800

    [文件新增]初始化配置

---
+++ src/main.js
@@ -0,0 +1,11 @@
+/*
+ * @Email: deuntlt@gmail.com
+ * @Author: hogger
+ * @Github: xxxx
+ * @Date: 2022-04-23 15:26:21
+ * @Description: screeps 入口文件
+ */
+//游戏入口函数
+export const loop = function () {
+    console.log('hello world')
+}
